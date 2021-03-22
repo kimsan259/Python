20a60fda81ebebb9a304d04967222aeb4b2909fb
kimsan4516 <kimsan4516@naver.com>
2021-03-23 02:33:51 +0900

    추가

---
+++ 구현/A+B-2_2558.py
@@ -0,0 +1,3 @@
+a = int(input()) # 아예 input을 받을때 정수형 자료형으로 형변환해버리는 방법
+b = int(input())
+print(a+b)
---
+++ 구현/A-B.py
@@ -0,0 +1,4 @@
+A, B = input().split() #입력되는 문자를 input()함수로 입력받고 split()함수로 나누어 A,B 변수에 저장
+A = int(A)
+B = int(B)
+print(A-B)
