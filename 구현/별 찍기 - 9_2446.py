3d85bd77da6ad028b63b423ae9975748c4f9fd46
kimsan4516 <kimsan4516@naver.com>
2022-02-01 23:17:26 +0900

    미완료

--- 구현/별 찍기 - 9_2446.py
+++ 구현/별 찍기 - 9_2446.py
@@ -1,5 +1,11 @@
+# n = int(input())
+#for i in range(n):
+#    print(" " * i + "*" * ((n - i) * 2 - 1))
+#for i in range(n - 2, -1, -1):
+#    print(" " * i + "*" * ((n - i) * 2 - 1))
+
 n = int(input())
-for i in range(n):
-    print(" " * i + "*" * ((n - i) * 2 - 1))
-for i in range(n - 2, -1, -1):
-    print(" " * i + "*" * ((n - i) * 2 - 1))
+for i in range(n,0,-1):
+    print(' ' * (n-i) + '*' * 2*i-1)
+for i in range(1,n):
+    print(' ' * (n-i) + '*' * 2*i-1)
