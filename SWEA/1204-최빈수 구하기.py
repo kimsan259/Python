341ac36f09562d2734e704256df4bb8a68b80835
kimsan4516 <kimsan4516@naver.com>
2022-02-17 20:11:04 +0900

    swea

--- SWEA/1204-최빈수 구하기.py
+++ SWEA/1204-최빈수 구하기.py
@@ -1,20 +1,9 @@
-t = int(input())
-while t:
-    #반복횟수를 감소시킴(반복문으로 처리해도 무방함)
-    t -= 1
-    #몇번째 케이스인지
-    case = int(input())
-    grade = list(map(int, input().split()))
-    lst = [0]*101
-    #각 자리수마다 카운팅한다.
-    #숫자 1번이 나오면 1번 인덱스에 +1 이런식으로
-    for g in grade:
-        lst[g] += 1
-    #최대값을 찾는다.
-    max_num = max(list)
-    #최대값과 같은값을 뒤에서부터 찾는다.
-    #앞에서 부터 찾으면 정답이 틀릴 수 있다.
-    for i in range(100, -1, -1):
-        if max_num == lst[i]:
-            print(f"#{case} {i}")
-            break
+from collections import Counter
+ 
+T = int(input())
+for _ in range(1,T+1):
+    n = int(input())
+    
+    li = list(map(int, input().split()))
+    most_num = Counter(li).most_common()[0][0]
+    print("#"+str(n),str(most_num))
---
+++ SWEA/1284-수도 요금 경쟁.py
@@ -0,0 +1,14 @@
+T = int(input())
+for tc in range(1, T+1):
+    p,q,r,s,w = map(int, input().split())
+    Asudo = p*w
+    Bsudo = 0
+    if w < r:
+        Bsudo = q
+    else:
+        Bsudo = q + (w-r)*s
+    
+    if Asudo < Bsudo:
+        print('#{} {}'.format(tc,Asudo))
+    else:
+        print('#{} {}'.format(tc,Bsudo))
